7cc58008623a113ba897f421f094ce63f7c2ca27
VadimTolstov <tolstov50@mail.ru>
2026-06-15 16:36:49 +0300

    Домашнее задание: Решение проблем

--- src/main/java/ru/tolstov/hw11/DebugTask10.java
+++ src/main/java/ru/tolstov/hw11/DebugTask10.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class DebugTask10 {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>(Arrays.asList("Alice", "Bob", "Charlie"));
-        for (int i = 0; i < names.size(); i++) {
+        for (int i = names.size() - 1 ; i >= 0 ; i--) {
             if (names.get(i).startsWith("A")) {
                 names.remove(i);
             }
